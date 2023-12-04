f3d992ee0985e261f05f53e8a7d5f117314edd2f
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2023-12-04 21:54:41 +0300

    commit

---
+++ src/modules/auth.ts
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET
+  );
+
+  return token;
+};
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer) {
+    res.status(401).json({ message: "not authorized" });
+  }
+
+  const [, token] = bearer.split(" ");
+
+  if (!token) {
+    res.status(401).json({ message: "Token is invalid." });
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "token is not valid" });
+    console.error(error);
+  }
+};
