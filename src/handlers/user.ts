115c0a92fd9fb23e7d45c0da141d0d2e95efc018
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2023-12-05 00:31:33 +0300

    user authentication process done.

---
+++ src/handlers/user.ts
@@ -0,0 +1,34 @@
+import prisma from "../database";
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
+
+export const createNewUser = async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      username: req.body.username,
+      password: await hashPassword(req.body.password),
+    },
+  });
+
+  const token = createJWT(user);
+  res.json({
+    username: req.body.username,
+    token: token,
+  });
+};
+
+export const signIn = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+    },
+  });
+
+  const isValid = await comparePasswords(req.body.password, user.password);
+  if (!isValid) {
+    res.status(401).json({ message: "Passwords did not match." });
+    return;
+  }
+
+  const token = createJWT(user);
+  res.json({ username: req.body.username, token });
+};
