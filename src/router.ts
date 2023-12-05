8f49266049804296e8b2dec92f590572afe0ad85
Armağan Erdem Taş <65099385+armagantas@users.noreply.github.com>
2023-12-06 01:04:37 +0300

    handler applications done.

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { body, validationResult } from "express-validator";
+import { body, oneOf, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
 
@@ -8,29 +9,58 @@ router.get("/product", (req, res) => {
   res.json({ message: "hello" });
 });
 router.get("/product/:id", () => {});
-router.put("/product/:id", body("name").isString(), (req, res) => {
-  const errors = validationResult(req);
-  console.log(errors);
-
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-  }
-});
-router.post("/product", () => {});
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
+router.post(
+  "/product",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
 router.delete("/product/:id", () => {});
 
 // update routes
 router.get("/update", () => {});
 router.get("/update/:id", () => {});
-router.put("/update/:id", () => {});
-router.post("/update", () => {});
+router.put(
+  "/update/:id",
+  body("title").optional(),
+  body("body").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
+  body("version").optional(),
+  (req, res) => {
+    // Gerçekleştirilecek işlemler
+  }
+);
+
+router.post(
+  "/update",
+  body("title").exists().isString(),
+  body("body").exists().isString(),
+  () => {}
+);
 router.delete("/update/:id", () => {});
 
 //update point routes
 router.get("/updatepoint", () => {});
 router.get("/updatepoint/:id", () => {});
-router.put("/updatepoint/:id", () => {});
-router.post("/updatepoint", () => {});
+router.put(
+  "/updatepoint/:id",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  () => {}
+);
+router.post(
+  "/updatepoint",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  body("updateId").exists().isString(),
+  () => {}
+);
 router.delete("/updatepoint/:id", () => {});
 
 export default router;
